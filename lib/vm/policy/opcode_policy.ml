04526726248f38572df23a6c548ae4b4456b265c
tomismeta <tomismeta@users.noreply.github.com>
2026-07-21 10:51:03 -0600

    vm: centralize opcode admission policy

--- lib/vm/policy/opcode_policy.ml
+++ lib/vm/policy/opcode_policy.ml
@@ -3,45 +3,306 @@
 
 module VM = Contract_vm
 
+type admission_class =
+  | Legacy_and_program
+  | Program_only
+  | Consensus_unsafe
+
+type info = {
+  name : string;
+  admission_class : admission_class;
+}
+
 type host_float_hit = {
   pc : int;
   opcode : string;
 }
 
-let host_float_opcode = function
-  | VM.EXP_LUT _ -> Some "EXP_LUT"
-  | VM.SOFTMAX_INPLACE _ -> Some "SOFTMAX_INPLACE"
-  | VM.LAYERNORM_INPLACE _ -> Some "LAYERNORM_INPLACE"
-  | VM.RMSNORM_INPLACE _ -> Some "RMSNORM_INPLACE"
-  | VM.SILU_INPLACE _ -> Some "SILU_INPLACE"
-  | VM.ROPE_APPLY _ -> Some "ROPE_APPLY"
-  | VM.MATMUL_FP _ -> Some "MATMUL_FP"
-  | VM.RMSNORM_FP _ -> Some "RMSNORM_FP"
-  | VM.SILU_FP _ -> Some "SILU_FP"
-  | VM.ELEMWISE_MUL_FP _ -> Some "ELEMWISE_MUL_FP"
-  | VM.RESIDUAL_ADD_FP _ -> Some "RESIDUAL_ADD_FP"
-  | VM.ROPE_APPLY_FP _ -> Some "ROPE_APPLY_FP"
-  | VM.LOAD_INT8_FP _ -> Some "LOAD_INT8_FP"
-  | VM.VECDOT_FP _ -> Some "VECDOT_FP"
-  | VM.ARGMAX_FP _ -> Some "ARGMAX_FP"
-  | VM.ATTENTION_KV_FP _ -> Some "ATTENTION_KV_FP"
-  | VM.APPEND_VEC_FP _ -> Some "APPEND_VEC_FP"
+let opcode_name = function
+  | VM.ADD _ -> "ADD"
+  | VM.SUB _ -> "SUB"
+  | VM.MUL _ -> "MUL"
+  | VM.DIV _ -> "DIV"
+  | VM.MOD _ -> "MOD"
+  | VM.NEG _ -> "NEG"
+  | VM.ABS _ -> "ABS"
+  | VM.EQ _ -> "EQ"
+  | VM.LT _ -> "LT"
+  | VM.GT _ -> "GT"
+  | VM.NEQ _ -> "NEQ"
+  | VM.LDI _ -> "LDI"
+  | VM.MOV _ -> "MOV"
+  | VM.SLOAD _ -> "SLOAD"
+  | VM.SSTORE _ -> "SSTORE"
+  | VM.SDEL _ -> "SDEL"
+  | VM.SLOADK _ -> "SLOADK"
+  | VM.SSTOREK _ -> "SSTOREK"
+  | VM.SDELK _ -> "SDELK"
+  | VM.MLOAD _ -> "MLOAD"
+  | VM.MSTORE _ -> "MSTORE"
+  | VM.JMP _ -> "JMP"
+  | VM.JIF _ -> "JIF"
+  | VM.JDEST _ -> "JDEST"
+  | VM.STOP -> "STOP"
+  | VM.REVERT -> "REVERT"
+  | VM.CALLER _ -> "CALLER"
+  | VM.ORIGIN _ -> "ORIGIN"
+  | VM.SELF _ -> "SELF"
+  | VM.EPOCH _ -> "EPOCH"
+  | VM.EPOCH_TIME _ -> "EPOCH_TIME"
+  | VM.VALUE _ -> "VALUE"
+  | VM.BALANCE _ -> "BALANCE"
+  | VM.TREEHASH _ -> "TREEHASH"
+  | VM.NODEID _ -> "NODEID"
+  | VM.TXHASH _ -> "TXHASH"
+  | VM.XCALL _ -> "XCALL"
+  | VM.SPAWN _ -> "SPAWN"
+  | VM.SPAWN2 _ -> "SPAWN2"
+  | VM.TRANSFER _ -> "TRANSFER"
+  | VM.CHECKPOINT -> "CHECKPOINT"
+  | VM.ROLLBACK -> "ROLLBACK"
+  | VM.COMMIT -> "COMMIT"
+  | VM.EMIT _ -> "EMIT"
+  | VM.CONCAT _ -> "CONCAT"
+  | VM.STRLEN _ -> "STRLEN"
+  | VM.ASSERT _ -> "ASSERT"
+  | VM.EFFORT _ -> "EFFORT"
+  | VM.NOP -> "NOP"
+  | VM.FHE_LOAD_PK _ -> "FHE_LOAD_PK"
+  | VM.FHE_ADD _ -> "FHE_ADD"
+  | VM.FHE_SUB _ -> "FHE_SUB"
+  | VM.FHE_MUL _ -> "FHE_MUL"
+  | VM.FHE_SCALE _ -> "FHE_SCALE"
+  | VM.FHE_DIV_CONST _ -> "FHE_DIV_CONST"
+  | VM.FHE_ADD_CONST _ -> "FHE_ADD_CONST"
+  | VM.FHE_SUB_CONST _ -> "FHE_SUB_CONST"
+  | VM.FHE_VERIFY_ZERO _ -> "FHE_VERIFY_ZERO"
+  | VM.FHE_VERIFY_RANGE _ -> "FHE_VERIFY_RANGE"
+  | VM.FHE_VERIFY_BOUND _ -> "FHE_VERIFY_BOUND"
+  | VM.GROTH16_VERIFY_BN254 _ -> "GROTH16_VERIFY_BN254"
+  | VM.FHE_COMMIT _ -> "FHE_COMMIT"
+  | VM.FHE_PEDERSEN _ -> "FHE_PEDERSEN"
+  | VM.FHE_SER _ -> "FHE_SER"
+  | VM.FHE_DESER _ -> "FHE_DESER"
+  | VM.FHE_SER_PK _ -> "FHE_SER_PK"
+  | VM.FHE_DESER_PK _ -> "FHE_DESER_PK"
+  | VM.CALL_INT _ -> "CALL_INT"
+  | VM.MLOADR _ -> "MLOADR"
+  | VM.MSTORER _ -> "MSTORER"
+  | VM.PARSE_INTS _ -> "PARSE_INTS"
+  | VM.ISADDR _ -> "ISADDR"
+  | VM.ISHEX _ -> "ISHEX"
+  | VM.STATE_PATH_KEY _ -> "STATE_PATH_KEY"
+  | VM.OBJECT_MEMBER_COUNT _ -> "OBJECT_MEMBER_COUNT"
+  | VM.OBJECT_HAS_MEMBER _ -> "OBJECT_HAS_MEMBER"
+  | VM.OBJECT_MEMBER_REF_AT _ -> "OBJECT_MEMBER_REF_AT"
+  | VM.OBJECT_TRANSITION_APPLY _ -> "OBJECT_TRANSITION_APPLY"
+  | VM.ASSERT_ADDR _ -> "ASSERT_ADDR"
+  | VM.SUBSTR _ -> "SUBSTR"
+  | VM.INDEXOF _ -> "INDEXOF"
+  | VM.SHA256 _ -> "SHA256"
+  | VM.KECCAK256 _ -> "KECCAK256"
+  | VM.ED25519_OK _ -> "ED25519_OK"
+  | VM.BITAND _ -> "BITAND"
+  | VM.BITOR _ -> "BITOR"
+  | VM.BITXOR _ -> "BITXOR"
+  | VM.BITSHL _ -> "BITSHL"
+  | VM.BITSHR _ -> "BITSHR"
+  | VM.SKEYS _ -> "SKEYS"
+  | VM.SKEYS_PAGE _ -> "SKEYS_PAGE"
+  | VM.SLOADN _ -> "SLOADN"
+  | VM.SSTOREN _ -> "SSTOREN"
+  | VM.FSTORE _ -> "FSTORE"
+  | VM.FLOAD _ -> "FLOAD"
+  | VM.MATMUL _ -> "MATMUL"
+  | VM.VECDOT _ -> "VECDOT"
+  | VM.VECDOT_Q16 _ -> "VECDOT_Q16"
+  | VM.EXP_LUT _ -> "EXP_LUT"
+  | VM.EXP_Q16 _ -> "EXP_Q16"
+  | VM.SOFTMAX_INPLACE _ -> "SOFTMAX_INPLACE"
+  | VM.SOFTMAX_Q16_INPLACE _ -> "SOFTMAX_Q16_INPLACE"
+  | VM.LAYERNORM_INPLACE _ -> "LAYERNORM_INPLACE"
+  | VM.LAYERNORM_Q16_INPLACE _ -> "LAYERNORM_Q16_INPLACE"
+  | VM.RELU_INPLACE _ -> "RELU_INPLACE"
+  | VM.RMSNORM_INPLACE _ -> "RMSNORM_INPLACE"
+  | VM.RMSNORM_Q16_INPLACE _ -> "RMSNORM_Q16_INPLACE"
+  | VM.SILU_INPLACE _ -> "SILU_INPLACE"
+  | VM.SILU_Q16_INPLACE _ -> "SILU_Q16_INPLACE"
+  | VM.ELEMWISE_MUL_INPLACE _ -> "ELEMWISE_MUL_INPLACE"
+  | VM.ELEMWISE_MUL_Q16 _ -> "ELEMWISE_MUL_Q16"
+  | VM.LOAD_INT8_BYTES_TO_MEM _ -> "LOAD_INT8_BYTES_TO_MEM"
+  | VM.RESIDUAL_ADD _ -> "RESIDUAL_ADD"
+  | VM.RESIDUAL_ADD_Q16 _ -> "RESIDUAL_ADD_Q16"
+  | VM.ROPE_APPLY _ -> "ROPE_APPLY"
+  | VM.ROPE_APPLY_Q16 _ -> "ROPE_APPLY_Q16"
+  | VM.LOAD_INT8_B64_TO_MEM _ -> "LOAD_INT8_B64_TO_MEM"
+  | VM.LOAD_INT8_Q16 _ -> "LOAD_INT8_Q16"
+  | VM.APPEND_VEC_Q16 _ -> "APPEND_VEC_Q16"
+  | VM.ARGMAX_Q16 _ -> "ARGMAX_Q16"
+  | VM.MATMUL_Q16 _ -> "MATMUL_Q16"
+  | VM.SHIFT_ROUND_INPLACE _ -> "SHIFT_ROUND_INPLACE"
+  | VM.MATMUL_FP _ -> "MATMUL_FP"
+  | VM.RMSNORM_FP _ -> "RMSNORM_FP"
+  | VM.SILU_FP _ -> "SILU_FP"
+  | VM.ELEMWISE_MUL_FP _ -> "ELEMWISE_MUL_FP"
+  | VM.RESIDUAL_ADD_FP _ -> "RESIDUAL_ADD_FP"
+  | VM.ROPE_APPLY_FP _ -> "ROPE_APPLY_FP"
+  | VM.LOAD_INT8_FP _ -> "LOAD_INT8_FP"
+  | VM.VECDOT_FP _ -> "VECDOT_FP"
+  | VM.ARGMAX_FP _ -> "ARGMAX_FP"
+  | VM.ATTENTION_KV_FP _ -> "ATTENTION_KV_FP"
+  | VM.ATTENTION_KV_Q16 _ -> "ATTENTION_KV_Q16"
+  | VM.APPEND_VEC_FP _ -> "APPEND_VEC_FP"
+
+let admission_class = function
+  | VM.EXP_LUT _
+  | VM.SOFTMAX_INPLACE _
+  | VM.LAYERNORM_INPLACE _
+  | VM.RMSNORM_INPLACE _
+  | VM.SILU_INPLACE _
+  | VM.ROPE_APPLY _
+  | VM.MATMUL_FP _
+  | VM.RMSNORM_FP _
+  | VM.SILU_FP _
+  | VM.ELEMWISE_MUL_FP _
+  | VM.RESIDUAL_ADD_FP _
+  | VM.ROPE_APPLY_FP _
+  | VM.LOAD_INT8_FP _
+  | VM.VECDOT_FP _
+  | VM.ARGMAX_FP _
+  | VM.ATTENTION_KV_FP _
+  | VM.APPEND_VEC_FP _ -> Consensus_unsafe
+  | VM.EXP_Q16 _
+  | VM.SOFTMAX_Q16_INPLACE _
+  | VM.LAYERNORM_Q16_INPLACE _
+  | VM.RMSNORM_Q16_INPLACE _
+  | VM.SILU_Q16_INPLACE _
+  | VM.ROPE_APPLY_Q16 _
+  | VM.ATTENTION_KV_Q16 _
+  | VM.VECDOT_Q16 _
+  | VM.ELEMWISE_MUL_Q16 _
+  | VM.RESIDUAL_ADD_Q16 _
+  | VM.LOAD_INT8_Q16 _
+  | VM.APPEND_VEC_Q16 _
+  | VM.ARGMAX_Q16 _ -> Program_only
+  | VM.ADD _
+  | VM.SUB _
+  | VM.MUL _
+  | VM.DIV _
+  | VM.MOD _
+  | VM.NEG _
+  | VM.ABS _
+  | VM.EQ _
+  | VM.LT _
+  | VM.GT _
+  | VM.NEQ _
+  | VM.LDI _
+  | VM.MOV _
+  | VM.SLOAD _
+  | VM.SSTORE _
+  | VM.SDEL _
+  | VM.SLOADK _
+  | VM.SSTOREK _
+  | VM.SDELK _
+  | VM.MLOAD _
+  | VM.MSTORE _
+  | VM.JMP _
+  | VM.JIF _
+  | VM.JDEST _
+  | VM.STOP
+  | VM.REVERT
+  | VM.CALLER _
+  | VM.ORIGIN _
+  | VM.SELF _
+  | VM.EPOCH _
+  | VM.EPOCH_TIME _
+  | VM.VALUE _
+  | VM.BALANCE _
+  | VM.TREEHASH _
+  | VM.NODEID _
+  | VM.TXHASH _
+  | VM.XCALL _
+  | VM.SPAWN _
+  | VM.SPAWN2 _
+  | VM.TRANSFER _
+  | VM.CHECKPOINT
+  | VM.ROLLBACK
+  | VM.COMMIT
+  | VM.EMIT _
+  | VM.CONCAT _
+  | VM.STRLEN _
+  | VM.ASSERT _
+  | VM.EFFORT _
+  | VM.NOP
+  | VM.FHE_LOAD_PK _
+  | VM.FHE_ADD _
+  | VM.FHE_SUB _
+  | VM.FHE_MUL _
+  | VM.FHE_SCALE _
+  | VM.FHE_DIV_CONST _
+  | VM.FHE_ADD_CONST _
+  | VM.FHE_SUB_CONST _
+  | VM.FHE_VERIFY_ZERO _
+  | VM.FHE_VERIFY_RANGE _
+  | VM.FHE_VERIFY_BOUND _
+  | VM.GROTH16_VERIFY_BN254 _
+  | VM.FHE_COMMIT _
+  | VM.FHE_PEDERSEN _
+  | VM.FHE_SER _
+  | VM.FHE_DESER _
+  | VM.FHE_SER_PK _
+  | VM.FHE_DESER_PK _
+  | VM.CALL_INT _
+  | VM.MLOADR _
+  | VM.MSTORER _
+  | VM.PARSE_INTS _
+  | VM.ISADDR _
+  | VM.ISHEX _
+  | VM.STATE_PATH_KEY _
+  | VM.OBJECT_MEMBER_COUNT _
+  | VM.OBJECT_HAS_MEMBER _
+  | VM.OBJECT_MEMBER_REF_AT _
+  | VM.OBJECT_TRANSITION_APPLY _
+  | VM.ASSERT_ADDR _
+  | VM.SUBSTR _
+  | VM.INDEXOF _
+  | VM.SHA256 _
+  | VM.KECCAK256 _
+  | VM.ED25519_OK _
+  | VM.BITAND _
+  | VM.BITOR _
+  | VM.BITXOR _
+  | VM.BITSHL _
+  | VM.BITSHR _
+  | VM.SKEYS _
+  | VM.SKEYS_PAGE _
+  | VM.SLOADN _
+  | VM.SSTOREN _
+  | VM.FSTORE _
+  | VM.FLOAD _
+  | VM.MATMUL _
+  | VM.VECDOT _
+  | VM.RELU_INPLACE _
+  | VM.ELEMWISE_MUL_INPLACE _
+  | VM.LOAD_INT8_BYTES_TO_MEM _
+  | VM.RESIDUAL_ADD _
+  | VM.LOAD_INT8_B64_TO_MEM _
+  | VM.MATMUL_Q16 _
+  | VM.SHIFT_ROUND_INPLACE _ -> Legacy_and_program
+
+let describe op = {
+  name = opcode_name op;
+  admission_class = admission_class op;
+}
+
+let host_float_opcode op =
+  match describe op with
+  | { name; admission_class = Consensus_unsafe } -> Some name
   | _ -> None
 
-let program_only_opcode = function
-  | VM.EXP_Q16 _ -> Some "EXP_Q16"
-  | VM.SOFTMAX_Q16_INPLACE _ -> Some "SOFTMAX_Q16_INPLACE"
-  | VM.LAYERNORM_Q16_INPLACE _ -> Some "LAYERNORM_Q16_INPLACE"
-  | VM.RMSNORM_Q16_INPLACE _ -> Some "RMSNORM_Q16_INPLACE"
-  | VM.SILU_Q16_INPLACE _ -> Some "SILU_Q16_INPLACE"
-  | VM.ROPE_APPLY_Q16 _ -> Some "ROPE_APPLY_Q16"
-  | VM.ATTENTION_KV_Q16 _ -> Some "ATTENTION_KV_Q16"
-  | VM.VECDOT_Q16 _ -> Some "VECDOT_Q16"
-  | VM.ELEMWISE_MUL_Q16 _ -> Some "ELEMWISE_MUL_Q16"
-  | VM.RESIDUAL_ADD_Q16 _ -> Some "RESIDUAL_ADD_Q16"
-  | VM.LOAD_INT8_Q16 _ -> Some "LOAD_INT8_Q16"
-  | VM.APPEND_VEC_Q16 _ -> Some "APPEND_VEC_Q16"
-  | VM.ARGMAX_Q16 _ -> Some "ARGMAX_Q16"
+let program_only_opcode op =
+  match describe op with
+  | { name; admission_class = Program_only } -> Some name
   | _ -> None
 
 let uses_host_float op =
---
+++ test/vm_opcode_policy_test.ml
@@ -0,0 +1,144 @@
+(*
+Octra Labs 2026
+
+Lite node, for internal use only (pre-release build 0x1067dzc2)
+
+Include at startup:
+- compiler
+- env-constructor
+- binary-proto consensus for updates
+- PVAC (optimized version, build 0f24dd-2025)
+- libp2p
+- gRPC (version 9738fdy44-2025)
+*)
+
+
+module VM = Contract_vm
+module Policy = Opcode_policy
+
+let check label condition =
+  if not condition then failwith label
+
+let check_info label expected_class expected_name op =
+  let info = Policy.describe op in
+  check (label ^ ": name") (String.equal info.Policy.name expected_name);
+  check (label ^ ": class") (info.Policy.admission_class = expected_class)
+
+let check_host_float label expected_name op =
+  check_info label Policy.Consensus_unsafe expected_name op;
+  check (label ^ ": host")
+    (Policy.host_float_opcode op = Some expected_name);
+  check (label ^ ": program-only")
+    (Policy.program_only_opcode op = None);
+  check (label ^ ": uses host float")
+    (Policy.uses_host_float op)
+
+let check_program_only label expected_name op =
+  check_info label Policy.Program_only expected_name op;
+  check (label ^ ": host")
+    (Policy.host_float_opcode op = None);
+  check (label ^ ": program-only")
+    (Policy.program_only_opcode op = Some expected_name);
+  check (label ^ ": uses host float")
+    (not (Policy.uses_host_float op))
+
+let check_legacy_and_program label expected_name op =
+  check_info label Policy.Legacy_and_program expected_name op;
+  check (label ^ ": host")
+    (Policy.host_float_opcode op = None);
+  check (label ^ ": program-only")
+    (Policy.program_only_opcode op = None);
+  check (label ^ ": uses host float")
+    (not (Policy.uses_host_float op))
+
+let host_float_cases = [
+  ("exp", "EXP_LUT", VM.EXP_LUT (0, 1));
+  ("softmax", "SOFTMAX_INPLACE", VM.SOFTMAX_INPLACE (0, 1));
+  ("layernorm", "LAYERNORM_INPLACE", VM.LAYERNORM_INPLACE (0, 1, 2, 3));
+  ("rmsnorm", "RMSNORM_INPLACE", VM.RMSNORM_INPLACE (0, 1, 2));
+  ("silu", "SILU_INPLACE", VM.SILU_INPLACE (0, 1));
+  ("rope", "ROPE_APPLY", VM.ROPE_APPLY (0, 1, 2, 3));
+  ("matmul-fp", "MATMUL_FP", VM.MATMUL_FP (0, 1, 2, 3, 4, 5));
+  ("rmsnorm-fp", "RMSNORM_FP", VM.RMSNORM_FP (0, 1, 2));
+  ("silu-fp", "SILU_FP", VM.SILU_FP (0, 1));
+  ("mul-fp", "ELEMWISE_MUL_FP", VM.ELEMWISE_MUL_FP (0, 1, 2));
+  ("residual-fp", "RESIDUAL_ADD_FP", VM.RESIDUAL_ADD_FP (0, 1, 2));
+  ("rope-fp", "ROPE_APPLY_FP", VM.ROPE_APPLY_FP (0, 1, 2, 3));
+  ("load-int8-fp", "LOAD_INT8_FP", VM.LOAD_INT8_FP (0, 1, 2, 3, 4));
+  ("vecdot-fp", "VECDOT_FP", VM.VECDOT_FP (0, 1, 2, 3));
+  ("argmax-fp", "ARGMAX_FP", VM.ARGMAX_FP (0, 1, 2));
+  ("attention-fp", "ATTENTION_KV_FP",
+   VM.ATTENTION_KV_FP (0, 1, 2, 3, 4, 5, 6, 7));
+  ("append-fp", "APPEND_VEC_FP", VM.APPEND_VEC_FP (0, 1, 2, 3));
+]
+
+let program_only_cases = [
+  ("exp-q16", "EXP_Q16", VM.EXP_Q16 (0, 1));
+  ("softmax-q16", "SOFTMAX_Q16_INPLACE", VM.SOFTMAX_Q16_INPLACE (0, 1));
+  ("layernorm-q16", "LAYERNORM_Q16_INPLACE",
+   VM.LAYERNORM_Q16_INPLACE (0, 1, 2, 3));
+  ("rmsnorm-q16", "RMSNORM_Q16_INPLACE", VM.RMSNORM_Q16_INPLACE (0, 1, 2));
+  ("silu-q16", "SILU_Q16_INPLACE", VM.SILU_Q16_INPLACE (0, 1));
+  ("rope-q16", "ROPE_APPLY_Q16", VM.ROPE_APPLY_Q16 (0, 1, 2, 3));
+  ("attention-q16", "ATTENTION_KV_Q16",
+   VM.ATTENTION_KV_Q16 (0, 1, 2, 3, 4, 5, 6, 7));
+  ("vecdot-q16", "VECDOT_Q16", VM.VECDOT_Q16 (0, 1, 2, 3));
+  ("mul-q16", "ELEMWISE_MUL_Q16", VM.ELEMWISE_MUL_Q16 (0, 1, 2));
+  ("residual-q16", "RESIDUAL_ADD_Q16", VM.RESIDUAL_ADD_Q16 (0, 1, 2));
+  ("load-int8-q16", "LOAD_INT8_Q16", VM.LOAD_INT8_Q16 (0, 1, 2, 3, 4));
+  ("append-q16", "APPEND_VEC_Q16", VM.APPEND_VEC_Q16 (0, 1, 2, 3));
+  ("argmax-q16", "ARGMAX_Q16", VM.ARGMAX_Q16 (0, 1, 2));
+]
+
+let legacy_and_program_cases = [
+  ("stop", "STOP", VM.STOP);
+  ("add", "ADD", VM.ADD (0, 1, 2));
+  ("storage-read", "SLOAD", VM.SLOAD (0, "k"));
+  ("fhe", "FHE_ADD", VM.FHE_ADD (0, 1, 2, 3));
+  ("blob-bytes", "LOAD_INT8_BYTES_TO_MEM",
+   VM.LOAD_INT8_BYTES_TO_MEM (0, 1, 2, 3, 4));
+  ("blob-b64", "LOAD_INT8_B64_TO_MEM",
+   VM.LOAD_INT8_B64_TO_MEM (0, 1, 2, 3, 4));
+  ("matmul-q16-outlier", "MATMUL_Q16", VM.MATMUL_Q16 (0, 1, 2, 3, 4, 5));
+  ("shift-round-outlier", "SHIFT_ROUND_INPLACE", VM.SHIFT_ROUND_INPLACE (0, 1, 2));
+]
+
+let check_legacy_error () =
+  let code = [|
+    VM.EXP_Q16 (0, 1);
+    VM.ATTENTION_KV_FP (0, 1, 2, 3, 4, 5, 6, 7);
+  |] in
+  match Policy.legacy_error code with
+  | Some (Policy.Program_only hit) ->
+    check "legacy error keeps Program-only precedence"
+      (hit.Policy.pc = 0 && String.equal hit.Policy.opcode "EXP_Q16")
+  | _ ->
+    failwith "legacy error keeps Program-only precedence"
+
+let check_first_host_float () =
+  let code = [|
+    VM.EXP_Q16 (0, 1);
+    VM.ATTENTION_KV_FP (0, 1, 2, 3, 4, 5, 6, 7);
+  |] in
+  match Policy.first_host_float code with
+  | Some hit ->
+    check "first host float skips Program-only opcodes"
+      (hit.Policy.pc = 1 && String.equal hit.Policy.opcode "ATTENTION_KV_FP")
+  | _ ->
+    failwith "first host float skips Program-only opcodes"
+
+let () =
+  List.iter
+    (fun (label, expected_name, op) ->
+      check_host_float label expected_name op)
+    host_float_cases;
+  List.iter
+    (fun (label, expected_name, op) ->
+      check_program_only label expected_name op)
+    program_only_cases;
+  List.iter
+    (fun (label, expected_name, op) ->
+      check_legacy_and_program label expected_name op)
+    legacy_and_program_cases;
+  check_legacy_error ();
+  check_first_host_float ()
